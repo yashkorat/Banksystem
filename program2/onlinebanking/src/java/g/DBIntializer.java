9b6e3ae8c11b8c981ae50ae40f9e062940aff88f
Yash Korat <koratyash2709@gmail.com>
2021-04-08 22:22:53 +0100

    change

--- program2/onlinebanking/src/java/g/DBIntializer.java
+++
@@ -1,8 +0,0 @@
-package g;
-
-public interface DBIntializer {
-String DRIVER="oracle.jdbc.driver.OracleDriver";
-String CON_STRING="jdbc:oracle:thin:@localhost:1521:xe";
-String USERNAME="system";
-String PASSWORD="oracle";
-}
